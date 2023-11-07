f69cb0d476b7e08a5c040094f6fa37a139dbb18d
NoelHuibers <noel@huibers.io>
2023-11-07 16:28:01 +0100

    FIX: Fixed solver with splr

--- backend/src/solver.rs
+++ backend/src/solver.rs
@@ -1,23 +1,9 @@
-//use minisat::Solver;
+use splr::*;
 
-//TODO: Find stable solution for all platforms.
-//fn solve(clauses: Vec<Vec<i32>>) {
-//    let mut solver = Solver::new();
-//    for clause in &clauses {
-//        solver.add_clause(clause);
-//    }
-//    let result = solver.solve();
-
-//    match result {
-//        Ok(true) => {
-//            let model = solver.get_model();
-//            println!("Solution: {:?}", model);
-//        }
-//        Ok(false) => {
-//            println!("No solution found. The puzzle is unsolvable.");
-//        }
-//        Err(err) => {
-//            eprintln!("An error occurred: {:?}", err);
-//        }
-//    }
-//}
+pub fn solve(clauses: Vec<Vec<i32>>) {
+    match Certificate::try_from(clauses) {
+        Ok(Certificate::SAT(ans)) => println!("s SATISFIABLE: {:?}", ans),
+        Ok(Certificate::UNSAT) => println!("s UNSATISFIABLE"),
+        Err(e) => panic!("s UNKNOWN; {}", e),
+    }
+}
