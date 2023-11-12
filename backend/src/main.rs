ef0d6fd2cd3d8af8e503d5b8073c0a4d21a31c1b
Florian Wuermseer <wuermsee@hm.edu>
2023-11-12 17:09:11 +0100

    feat: regenerate board from SAT solution

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -2,13 +2,16 @@ extern crate clap;
 
 use clap::{command, Arg};
 
-mod parse_input;
 mod generate_clauses;
+mod parse_input;
 mod solver;
 mod writer;
+mod reconstruct;
 
 use generate_clauses::generate;
 use parse_input::{parse_input, print_infos};
+
+use crate::writer::generate_dimacs;
 
 // To run an example from root: cargo run --package backend -- --mode [encode/solve] --input [FILE PATH]
 // Short: cargo run --package backend -- -m [encode/solve] -i [FILE PATH]
@@ -37,8 +40,14 @@ fn main() {
         "encode" => match parse_input(input_file) {
             Ok(game_board) => {
                 print_infos(&game_board);
-                let (clauses, _) = generate(&game_board);
+                let (clauses, var_map) = generate(&game_board);
                 print!("Clauses as Vec<Vec<i32>>: {:?}", clauses);
+                let out_file = &format!("{}.sat", input_file);
+                let dimacs_generated = generate_dimacs(&clauses, var_map.keys().len(), out_file);
+                match dimacs_generated {
+                    Ok(_) => println!("Successfully generated {}", out_file),
+                    Err(e) => eprint!("{}", e),
+                }
             }
             Err(err) => eprintln!("Error: {}", err),
         },
--- backend/src/writer.rs
+++ backend/src/writer.rs
@@ -1,9 +1,9 @@
 use std::fs::File;
 use std::io::{BufWriter, Write};
 
-fn generate_dimacs(
+pub fn generate_dimacs(
     clauses: &Vec<Vec<i32>>,
-    variables: i32,
+    variables: usize,
     output_filename: &str,
 ) -> std::io::Result<()> {
     let file = File::create(output_filename)?;
