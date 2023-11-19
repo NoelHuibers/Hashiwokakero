a02623286d18791bac991a7dd90a0d08031504da
Florian Wuermseer <wuermsee@hm.edu>
2023-11-19 17:16:39 +0100

    doc: show cnf results

--- backend/tests/integration_test.rs
+++ backend/tests/integration_test.rs
@@ -29,16 +29,17 @@ fn test_integration() {
 fn get_current_working_dir() -> std::io::Result<std::path::PathBuf> {
     std::env::current_dir()
 }
+
 #[test]
 fn test_integration() {
     for i in 1..=24 {
         println!("Test {}", i);
         let input_file = format!("./input/test{}.txt", i);
-        let output_file = format!("./output/test{}.txt", i);
+        let output_file = format!("../solutions/test{}.txt", i);
         match parse_input(&input_file) {
             Ok(game_board) => {
                 let (clauses, var_map) = generate(&game_board);
-                let out_file = &format!("./output/test{}.cnf", i);
+                let out_file = &format!("../solutions/test{}.cnf", i);
                 let dimacs_generated = generate_dimacs(&clauses, var_map.keys().len(), out_file);
                 match dimacs_generated {
                     Ok(_) => match solver::solve(&out_file) {
