7c1ef21a26008d039da1d0c2b2f3b0c99c22f834
NoelHuibers <noel@huibers.io>
2023-11-20 02:25:04 +0100

    Use an effect and signal to hold the Grid Using a Vec<Vec<i32>> to hold the data. Form to submit it.

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,4 @@
-use leptos::*;
+use leptos::{ev::SubmitEvent, *};
 use leptos_meta::*;
 use leptos_router::*;
 
@@ -31,6 +31,11 @@ fn HomePage() -> impl IntoView {
     let (rows, set_rows) = create_signal(3);
     let (columns, set_columns) = create_signal(3);
 
+    let (tableprop, set_tableprop) = create_signal(vec![vec![0; 3]; 3]);
+    create_effect(move |_| {
+        set_tableprop(vec![vec![0; columns.get() as usize]; rows.get() as usize]);
+    });
+
     view! {
         <main class="h-full w-full min-h-screen min-w-full flex flex-col items-center space-y-4 bg-slate-100 p-16">
             <h1 class="font-bold text-4xl text-violet-700">"Hashiwakakeru"</h1>
@@ -60,44 +65,79 @@ fn HomePage() -> impl IntoView {
 
             </div>
             <div class="h-fit w-fit items-center">
-                <Table rows=rows columns=columns/>
-            </div>
-            <div class="flex flex-row space-x-4">
-                <button class="w-24 h-12 rounded px-2 bg-violet-700 text-slate-100">
-                    "To CNF"
-                </button>
-                <button class="w-24 h-12 rounded px-2 bg-violet-700 text-slate-100">"Solve"</button>
-                <button class="w-24 h-12 rounded px-2 bg-violet-700 text-slate-100">
-                    "Generate"
-                </button>
-                <button class="w-24 h-12 rounded px-2 bg-violet-700 text-slate-100">"Clear"</button>
+                <Table rows=rows columns=columns tableprop=tableprop set_tableprop=set_tableprop/>
             </div>
         </main>
     }
 }
 
 #[component]
-fn Table(rows: ReadSignal<i32>, columns: ReadSignal<i32>) -> impl IntoView {
+fn Table(
+    rows: ReadSignal<i32>,
+    columns: ReadSignal<i32>,
+    tableprop: ReadSignal<Vec<Vec<i32>>>,
+    set_tableprop: WriteSignal<Vec<Vec<i32>>>,
+) -> impl IntoView {
+    let on_submit = move |ev: SubmitEvent| {
+        ev.prevent_default();
+        let submitid = ev.submitter().unwrap().id();
+        // TODO: Use the Vec<Vec<i32>> to generate the CNF or Solve the puzzle.
+        let testinput = tableprop.get();
+    };
     view! {
-        <div class="flex flex-col items-center">
+        <form on:submit=on_submit class="flex flex-col items-center">
             <For
                 each=move || (0..rows.get()).into_iter()
                 key=|counter| *counter
                 children=move |id| {
                     view! {
-                        <div>
-                            <Column columns=columns.clone()/>
-                        </div>
+                        <Column
+                            columns=columns
+                            tableprop=tableprop
+                            set_tableprop=set_tableprop
+                            rowid=id
+                        />
                     }
                 }
             />
 
-        </div>
+            <div class="flex flex-row space-x-4 py-8">
+                <input
+                    type="submit"
+                    id="0"
+                    value="To CNF"
+                    class="w-24 h-12 rounded px-2 bg-violet-700 text-slate-100"
+                />
+                <input
+                    type="submit"
+                    id="1"
+                    value="Solve"
+                    class="w-24 h-12 rounded px-2 bg-violet-700 text-slate-100"
+                />
+                <input
+                    type="submit"
+                    id="2"
+                    value="Generate"
+                    class="w-24 h-12 rounded px-2 bg-violet-700 text-slate-100"
+                />
+                <input
+                    type="submit"
+                    id="3"
+                    value="Clear"
+                    class="w-24 h-12 rounded px-2 bg-violet-700 text-slate-100"
+                />
+            </div>
+        </form>
     }
 }
 
 #[component]
-fn Column(columns: ReadSignal<i32>) -> impl IntoView {
+fn Column(
+    columns: ReadSignal<i32>,
+    tableprop: ReadSignal<Vec<Vec<i32>>>,
+    set_tableprop: WriteSignal<Vec<Vec<i32>>>,
+    rowid: i32,
+) -> impl IntoView {
     view! {
         <div class="flex flex-row items-center">
             <For
@@ -109,7 +149,14 @@ fn Column(columns: ReadSignal<i32>) -> impl IntoView {
                             type="text"
                             min="0"
                             max="8"
-                            class="h-16 w-16 border bg-violet-700 p-2 justify-center text-slate-100 text-lg"
+                            class="h-16 w-16 border bg-violet-700 justify-center p-2 text-slate-100 text-lg"
+                            on:input=move |value| {
+                                let mut table = tableprop.get();
+                                table[id
+                                    as usize][rowid
+                                    as usize] = event_target_value(&value).parse().unwrap_or(0);
+                                set_tableprop(table);
+                            }
                         />
                     }
                 }
