cae51f1e5d16391a4c92da31db6961b238e19eb1
schroederl <laura.s.schroeder@gmx.de>
2023-11-21 19:57:01 +0100

    Refactoring + updated Readme + dummy file Eval

--- backend/benches/hashi_benchmark.rs
+++ backend/benches/hashi_benchmark.rs
@@ -1,9 +1,6 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use backend::parse_input::parse_input;
-use backend::generate_clauses::generate;
-use backend::{solver, reconstruct};
-use backend::writer::generate_dimacs;
-use std::fs;
+
+use backend::modes::{encode_mode, solve_mode, esr_mode};
 
 fn encode_benchmark(c: &mut Criterion) {
     let test_files = vec![
@@ -20,24 +17,7 @@ fn encode_benchmark(c: &mut Criterion) {
                 if !std::path::Path::new(input_file).exists() {
                     return; 
                 }
-                match black_box(parse_input(input_file)) {
-                    Ok(game_board) => {
-                        let (clauses, var_map) = generate(&game_board);
-                        let out_file = &format!("{}.cnf", input_file);
-                        let dimacs_generated = generate_dimacs(&clauses, var_map.keys().len(), out_file);
-                        match dimacs_generated {
-                            Ok(_) => {
-                                println!("Successfully generated {}", out_file);
-                                // Delete the generated file
-                                if let Err(e) = fs::remove_file(out_file) {
-                                    eprintln!("Failed to delete file: {}", e);
-                                }
-                            }
-                            Err(e) => eprint!("{}", e),
-                        }
-                    }
-                    Err(err) => eprintln!("Error: {}", err),
-                }
+                encode_mode(input_file.to_string());
             })
         });
     }
@@ -56,27 +36,10 @@ fn solve_benchmark(c: &mut Criterion) {
         c.bench_function(&benchmark_name, |b| {
             b.iter(|| {
                 if !std::path::Path::new(input_file).exists() {
-                    return; // Abbruch, wenn die Datei nicht gefunden wird
+                    return; 
                 }
                 let output_file = Some("/output/solution.txt");
-                match black_box(solver::solve(input_file)) {
-                    Ok(certificate) => match output_file {
-                        Some(output_file) => match solver::write_solution(certificate, output_file) {
-                            Ok(_) => {
-                                println!("Solution written to {}", output_file);
-                            }
-                            Err(err) => {
-                                eprintln!("Error: {}", err);
-                            }
-                        },
-                        None => {
-                            println!("Solution: {:?}", certificate);
-                        }
-                    },
-                    Err(err) => {
-                        eprintln!("Error: {}", err);
-                    }
-                }
+                solve_mode(input_file.to_string(), output_file.map(|s| s.to_string()));
             })
         });
     }
@@ -95,48 +58,15 @@ fn esr_benchmark(c: &mut Criterion) {
         c.bench_function(&benchmark_name, |b| {
             b.iter(|| {
                 if !std::path::Path::new(input_file).exists() {
-                    return; // Abbruch, wenn die Datei nicht gefunden wird
+                    return; 
                 }
                 let output_file = Some("/output/solution.txt");
-                match parse_input(input_file) {
-                    Ok(game_board) => {
-                        let (clauses, var_map) = generate(&game_board);
-                        let out_file = &format!("{}.cnf", input_file);
-                        let dimacs_generated = generate_dimacs(&clauses, var_map.keys().len(), out_file);
-                        match dimacs_generated {
-                            Ok(_) => match solver::solve(&out_file) {
-                                Ok(certificate) => match output_file {
-                                    Some(output_file) => {
-                                        match solver::write_solution(certificate, output_file) {
-                                            Ok(_) => {
-                                                reconstruct::reconstruct_puzzle(
-                                                    &output_file.to_string(),
-                                                    &var_map,
-                                                    &game_board,
-                                                );
-                                            }
-                                            Err(err) => {
-                                                eprintln!("Error: {}", err);
-                                            }
-                                        }
-                                    }
-                                    None => {
-                                        println!("Solution: {:?}", certificate);
-                                    }
-                                },
-                                Err(err) => {
-                                    eprintln!("Error: {}", err);
-                                }
-                            },
-                            Err(e) => eprint!("{}", e),
-                        }
-                    }
-                    Err(err) => eprintln!("Error: {}", err),
-                }
+                esr_mode(input_file.to_string(), output_file.map(|s| s.to_string()));
             })
         });
     }
 }
+
 
 fn main() {
     let mut criterion = Criterion::default();
