ae7a2438d4604f4bb3d51a3c8fabd0097714e2eb
schroederl <laura.s.schroeder@gmx.de>
2023-11-22 20:43:23 +0100

    Refactoring + updated Evaluation

--- backend/src/modes.rs
+++ backend/src/modes.rs
@@ -4,76 +4,78 @@ use crate::{reconstruct, solver};
 use crate::writer::generate_dimacs;
 
 pub fn encode_mode(input_file: String) {
-    match parse_input(&&input_file) {
-        Ok(game_board) => {
-            let (clauses, var_map) = generate(&game_board);
-            let out_file = &format!("{}.cnf", input_file);
-            let dimacs_generated = generate_dimacs(&clauses, var_map.keys().len(), out_file);
-            match dimacs_generated {
-                Ok(_) => println!("Successfully generated {}", out_file),
-                Err(e) => eprint!("{}", e),
-            }
+    let game_board = match parse_input(&input_file) {
+        Ok(game_board) => game_board,
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            return;
         }
-        Err(err) => eprintln!("Error: {}", err),
+    };
+
+    let (clauses, var_map) = generate(&game_board);
+    let out_file = format!("{}.cnf", input_file);
+    if let Err(e) = generate_dimacs(&clauses, var_map.keys().len(), &out_file) {
+        eprint!("{}", e);
+        return;
     }
+
+    println!("Successfully generated {}", out_file);
 }
 
 pub fn solve_mode(input_file: String, output_file: Option<String>) {
-    match solver::solve(&input_file) {
-        Ok(certificate) => match output_file {
-            Some(output_file) => match solver::write_solution(certificate, &output_file) {
-                Ok(_) => {
-                    println!("Solution written to {}", output_file);
-                }
-                Err(err) => {
-                    eprintln!("Error: {}", err);
-                }
-            },
-            None => {
-                println!("Solution: {:?}", certificate);
-            }
-        },
+    let certificate = match solver::solve(&input_file) {
+        Ok(certificate) => certificate,
         Err(err) => {
             eprintln!("Error: {}", err);
+            return;
         }
+    };
+
+    if let Some(output_file) = output_file {
+        if let Err(err) = solver::write_solution(certificate, &output_file) {
+            eprintln!("Error: {}", err);
+            return;
+        }
+
+        println!("Solution written to {}", output_file);
+    } else {
+        println!("Solution: {:?}", certificate);
     }
 }
 
 pub fn esr_mode(input_file: String, output_file: Option<String>) {
-    match parse_input(&input_file) {
-        Ok(game_board) => {
-            let (clauses, var_map) = generate(&game_board);
-            let out_file = &format!("{}.cnf", input_file);
-            let dimacs_generated = generate_dimacs(&clauses, var_map.keys().len(), out_file);
-            match dimacs_generated {
-                Ok(_) => match solver::solve(&out_file) {
-                    Ok(certificate) => match output_file {
-                        Some(output_file) => {
-                            match solver::write_solution(certificate, &output_file) {
-                                Ok(_) => {
-                                    let res = reconstruct::reconstruct_puzzle(
-                                        &output_file,
-                                        &var_map,
-                                        &game_board,
-                                    );
-                                    print!("{}", res);
-                                }
-                                Err(err) => {
-                                    eprintln!("Error: {}", err);
-                                }
-                            }
-                        }
-                        None => {
-                            println!("Solution: {:?}", certificate);
-                        }
-                    },
-                    Err(err) => {
-                        eprintln!("Error: {}", err);
-                    }
-                },
-                Err(e) => eprint!("{}", e),
-            }
+    let game_board = match parse_input(&input_file) {
+        Ok(game_board) => game_board,
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            return;
         }
-        Err(err) => eprintln!("Error: {}", err),
+    };
+
+    let (clauses, var_map) = generate(&game_board);
+    let out_file = format!("{}.cnf", input_file);
+    if let Err(e) = generate_dimacs(&clauses, var_map.keys().len(), &out_file) {
+        eprint!("{}", e);
+        return;
+    }
+
+    let certificate = match solver::solve(&out_file) {
+        Ok(certificate) => certificate,
+        Err(err) => {
+            eprintln!("Error: {}", err);
+            return;
+        }
+    };
+
+    if let Some(output_file) = output_file {
+        if let Err(err) = solver::write_solution(certificate, &output_file) {
+            eprintln!("Error: {}", err);
+            return;
+        }
+
+        let res = reconstruct::reconstruct_puzzle(&output_file, &var_map, &game_board);
+        print!("{}", res);
+    } else {
+        println!("Solution: {:?}", certificate);
     }
 }
